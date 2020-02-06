7111bd6323759b6defdb967dac72fd521f84a8d9
Michael-Xie <xiemicha@gmail.com>
2020-02-06 16:30:15 +0000

    Linked categories to nav bar and initiated index page for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,20 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    # @category = Product.new(product_params)
+
+    # if @category.save
+    #   redirect_to [:admin, :products], notice: 'Product created!'
+    # else
+    #   render :new
+    # end
   end
-  
+
 end
