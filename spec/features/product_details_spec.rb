9f45eb940f39fae4523dbf9ea67f3d5bd4eed45d
Michael-Xie <xiemicha@gmail.com>
2020-02-11 20:05:57 +0000

    Add feature spec for product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
-
-    10.times do |n|
+  
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -14,15 +12,16 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
         quantity: 10,
         price: 64.99
       )
-    end
   end
 
-  scenario "They see all products" do
+  scenario "They see product details" do
     visit root_path
+
+    click_link "Details"
+    expect(page).to have_content "Apparel »"
 
     # commented out b/c it's for debugging only
     # save_and_open_screenshot
-
-    expect(page).to have_css 'article.product', count: 10
   end
+
 end
