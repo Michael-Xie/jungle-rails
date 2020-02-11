4196b06153f47e1e03b9790e4ce2c0474a2fcc62
Michael-Xie <xiemicha@gmail.com>
2020-02-11 00:47:54 +0000

    Add requirement that first_name, last_name, and email are needed to create User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "should be created with password and password_confirmation fields" do
-    user = User.new({ password: "test", password_confirmation: "test" })
+    user = User.new({ password: "test", password_confirmation: "test" , first_name: "first", last_name: "last", email: "test@test.com"})
     expect(user).to be_valid
 
   end
   it "should not be created with non-matching password" do
-    user = User.new({ password: "test", password_confirmation: "wrong" })
+    user = User.new({ password: "test", password_confirmation: "wrong", first_name: "first", last_name: "last", email: "test@test.com"})
     expect(user).to_not be_valid
     expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
 
   end
 
   it "should have unique emails in database" do
-    user1 = User.new({email: "test@test.com", password: "test", password_confirmation: "test"})
+    user1 = User.new({email: "test@test.com", password: "test", password_confirmation: "test", first_name: "first", last_name: "last"})
     user1.save()
-    user2 = User.new({email: "test@test.com", password: "hi", password_confirmation: "hi"})
+    user2 = User.new({email: "test@test.com", password: "hi", password_confirmation: "hi", first_name: "first", last_name: "last"})
     # user2.save()
     expect(user2).to_not be_valid
     expect(user2.errors.full_messages).to include("Email has already been taken")
