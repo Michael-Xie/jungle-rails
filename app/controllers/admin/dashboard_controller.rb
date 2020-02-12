ac9508c58cf8a340a8662b3c5d81666c7ff4c9b0
Michael-Xie <xiemicha@gmail.com>
2020-02-12 00:39:42 +0000

    Update seed with product with no quantity left
    Update all admin page accesses to need admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
+
   def show
     @num_products = Product.count(:all)
     @num_categories = Category.count(:all)
